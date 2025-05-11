784666273ba98f169231e3e2ac29bc4e67eed355
ebuberoderick <ebuberoderick2@gmail.com>
2025-05-11 20:30:00 +0100

    vadj

--- src/components/molecules/SideNav.js
+++ src/components/molecules/SideNav.js
@@ -1,5 +1,5 @@
 import React from 'react'
-import img from "@asset/images/pic.png"
+import img from "../../images/pic.png"
 import Image from 'next/image'
 import AppLink from '../organisms/AppLink'
 import { FaCalendarAlt, FaUserAlt, FaCog } from "react-icons/fa";
