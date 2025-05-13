40bed01eca56374e183d185d61057e43b40b452d
ebuberoderick <ebuberoderick2@gmail.com>
2025-05-13 18:22:04 +0100

    fixed profile screen

--- src/pages/profile.jsx
+++ src/pages/profile.jsx
@@ -16,14 +16,38 @@ function Profile() {
   return (
 
     <AppLayout title="Profile">
-
-
       <div className="p-3 space-y-4">
         <div className="">
-          {editMode && <div onClick={() => setEditMode(false)} className="flex items-center gap-2 cursor-pointer"><TfiAngleLeft /> Go Back</div>}
+          {editMode && <div onClick={() => setEditMode(false)} className="flex items-center gap-2 pb-3 cursor-pointer"><TfiAngleLeft /> Go Back</div>}
           <div className="">
-            <div className="h-56 bg-amber-50 rounded-lg"></div>
-            <div className=""></div>
+            <div className="h-56 bg-amber-50 rounded-lg relative">
+              <div className="w-8 h-8 cursor-pointer bg-amber-500 rounded-full absolute bottom-4 right-4"></div>
+            </div>
+            <div className="py-6 space-y-7 lg:space-y-0 lg:flex pl-4 md:pl-16">
+              <div className="divide-x-2 flex-grow flex divide-amber-400">
+                <div className="relative w-48">
+                  <div className="absolute w-44 h-44 rounded-full -bottom-3">
+                    <div className="relative h-full w-full">
+                      <div className="w-full h-full overflow-hidden rounded-full bg-amber-200"></div>
+                      <div className="w-8 h-8 cursor-pointer bg-amber-500 rounded-full absolute bottom-4 right-4"></div>
+                    </div>
+                  </div>
+                </div>
+                <div className="font-semibold px-5">
+                  <div className="font-bold">Diala Victor</div>
+                  <div className="">+234 901827464</div>
+                  <div className="">email@gmail.com</div>
+                </div>
+              </div>
+              {
+                !editMode && (
+                  <div className="">
+                    <button onClick={() => setEditMode(true)} className="px-7 cursor-pointer text-xs disabled:cursor-none disabled:bg-amber-500/35 shadow-md bg-amber-500 text-white rounded-lg py-3">Edit Profile</button>
+                  </div>
+                )
+              }
+
+            </div>
           </div>
         </div>
         <div className="space-y-3 bg-white/60 border border-gray-200 p-5 rounded-xl">
@@ -32,11 +56,11 @@ function Profile() {
             <AppInput disabled={!editMode} placeholder="e.g Chisomaga" name="fname" required icon={<HiUser />} label="First Name" />
             <AppInput disabled={!editMode} placeholder="e.g Diala" name="lname" required icon={<HiUser />} label="Last Name" />
             <AppInput disabled={!editMode} placeholder="e.g 0901827464" name="phone" required icon={<ImPhone />} label="Phone" />
-            <AppInput placeholder="e.g email@gmail.com" name="email" disabled type={'mail'} icon={<MdMail />} required label="Email" />
-            <AppInput disabled={!editMode} placeholder="Country" name="country" icon={<FaMap />} required label="Country" />
+            <AppInput placeholder="e.g email@gmail.com" name="email" disabled type='mail' icon={<MdMail />} required label="Email" />
+            <AppInput disabled placeholder="Country" name="country" icon={<FaMap />} required label="Country" />
             <AppInput disabled={!editMode} placeholder="State" name="state" icon={<MdExplore />} required label="State" />
             <AppInput disabled={!editMode} placeholder="City" name="city" icon={<FaMapLocation />} required label="City" />
-            <AppInput disabled={!editMode} name="dob" icon={<FaCalendarAlt />} required label="Date of Birth" type={"date"} />
+            <AppInput disabled={!editMode} name="dob" icon={<FaCalendarAlt />} required label="Date of Birth" type="date" />
             {
               editMode && (
                 <div className="grid sm:grid-cols-2 gap-4">
