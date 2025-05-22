955380a138444b13c08c32342bdb681c90909a90
ebuberoderick <ebuberoderick2@gmail.com>
2025-05-22 20:50:07 +0100

    working on profile

--- src/pages/profile.jsx
+++ src/pages/profile.jsx
@@ -7,11 +7,26 @@ import { ImPhone } from "react-icons/im";
 import { FaMap, FaCalendarAlt } from "react-icons/fa";
 import { FaMapLocation } from "react-icons/fa6";
 import { TfiAngleLeft } from "react-icons/tfi";
+import { IoCameraOutline } from "react-icons/io5";
+import { useSelector } from 'react-redux';
+import serialize from '@/hooks/Serialize';
 
 function Profile() {
 
   const [editMode, setEditMode] = useState(false)
   const [proccessing, setProccessing] = useState(false)
+  const user = useSelector((state) => state.User.value?.user)
+
+
+  const update = async (e) => {
+    e.preventDefault()
+    const payload = serialize(e.target)
+    console.log(payload);
+  }
+
+
+  // console.log(user);
+
 
   return (
 
@@ -24,19 +39,21 @@ function Profile() {
               <div className="w-8 h-8 cursor-pointer bg-amber-500 rounded-full absolute bottom-4 right-4"></div>
             </div>
             <div className="py-6 space-y-7 lg:space-y-0 lg:flex pl-4 md:pl-16">
-              <div className="divide-x-2 flex-grow flex divide-amber-400">
+              <div className="sm:divide-x-2 flex-grow flex items-center divide-amber-400">
                 <div className="relative w-48">
-                  <div className="absolute w-44 h-44 rounded-full -bottom-3">
+                  <div className="sm:absolute w-32 h-32 md:w-44 md:h-44 rounded-full -bottom-3">
                     <div className="relative h-full w-full">
                       <div className="w-full h-full overflow-hidden rounded-full bg-amber-200"></div>
-                      <div className="w-8 h-8 cursor-pointer bg-amber-500 rounded-full absolute bottom-4 right-4"></div>
+                      <div className="w-8 h-8 flex items-center justify-center text-white cursor-pointer bg-amber-500 rounded-full absolute sm:bottom-4 bottom-2 right-2 sm:right-4">
+                        <IoCameraOutline />
+                      </div>
                     </div>
                   </div>
                 </div>
-                <div className="font-semibold px-5">
-                  <div className="font-bold">Diala Victor</div>
+                <div className="font-semibold text-xs px-5">
+                  <div className="font-bold">{user?.fname} {user?.lname}</div>
                   <div className="">+234 901827464</div>
-                  <div className="">email@gmail.com</div>
+                  <div className="">{user?.email}</div>
                 </div>
               </div>
               {
@@ -52,32 +69,34 @@ function Profile() {
         </div>
         <div className="space-y-3 bg-white/60 border border-gray-200 p-5 rounded-xl">
           <div className="font-bold">Personal Info</div>
-          <div className="grid sm:grid-cols-2 gap-4">
-            <AppInput disabled={!editMode} placeholder="e.g Chisomaga" name="fname" required icon={<HiUser />} label="First Name" />
-            <AppInput disabled={!editMode} placeholder="e.g Diala" name="lname" required icon={<HiUser />} label="Last Name" />
-            <AppInput disabled={!editMode} placeholder="e.g 0901827464" name="phone" required icon={<ImPhone />} label="Phone" />
-            <AppInput placeholder="e.g email@gmail.com" name="email" disabled type='mail' icon={<MdMail />} required label="Email" />
-            <AppInput disabled placeholder="Country" name="country" icon={<FaMap />} required label="Country" />
-            <AppInput disabled={!editMode} placeholder="State" name="state" icon={<MdExplore />} required label="State" />
-            <AppInput disabled={!editMode} placeholder="City" name="city" icon={<FaMapLocation />} required label="City" />
-            <AppInput disabled={!editMode} name="dob" icon={<FaCalendarAlt />} required label="Date of Birth" type="date" />
-            {
-              editMode && (
-                <div className="grid sm:grid-cols-2 gap-4">
-                  <div className="">
-                    <div className="flex">
-                      <div onClick={() => setEditMode(false)} className="px-4 cursor-pointer w-full text-center border border-amber-500 text-amber-500 rounded-lg py-3">Cancel</div>
-                    </div>
-                  </div>
-                  <div className="">
+          <form onSubmit={update}>
+            <div className="grid sm:grid-cols-2 gap-4">
+              <AppInput disabled={!editMode} defaultValue={user?.fname} placeholder="e.g Chisomaga" name="fname" required icon={<HiUser />} label="First Name" />
+              <AppInput disabled={!editMode} defaultValue={user?.lname} placeholder="e.g Diala" name="lname" required icon={<HiUser />} label="Last Name" />
+              <AppInput disabled={!editMode} defaultValue={user?.phone} placeholder="e.g 0901827464" name="phone" required icon={<ImPhone />} label="Phone" />
+              <AppInput placeholder="e.g email@gmail.com" defaultValue={user?.email} name="email" disabled type='mail' icon={<MdMail />} required label="Email" />
+              <AppInput disabled placeholder="Country" defaultValue={user?.country} name="country" icon={<FaMap />} required label="Country" />
+              <AppInput disabled={!editMode} placeholder="State" defaultValue={user?.state} name="state" icon={<MdExplore />} required label="State" />
+              <AppInput disabled={!editMode} placeholder="City" name="city" defaultValue={user?.city} icon={<FaMapLocation />} required label="City" />
+              <AppInput disabled={!editMode} name="dob" defaultValue={user?.dob} icon={<FaCalendarAlt />} required label="Date of Birth" type="date" />
+              {
+                editMode && (
+                  <div className="grid col-span-1 sm:col-span-2 lg:col-span-1 sm:grid-cols-2 gap-4">
                     <div className="">
-                      <button disabled={proccessing} className="px-7 w-full cursor-pointer disabled:cursor-none disabled:bg-amber-500/35 shadow-md bg-amber-500 text-white rounded-lg py-3"> {proccessing ? "Proccessing..." : "Save Changes"}</button>
+                      <div className="flex">
+                        <div onClick={() => setEditMode(false)} className="px-4 cursor-pointer w-full text-center border border-amber-500 text-amber-500 rounded-lg py-3">Cancel</div>
+                      </div>
+                    </div>
+                    <div className="">
+                      <div className="">
+                        <button disabled={proccessing} className="px-7 w-full cursor-pointer disabled:cursor-none disabled:bg-amber-500/35 shadow-md bg-amber-500 text-white rounded-lg py-3"> {proccessing ? "Proccessing..." : "Save Changes"}</button>
+                      </div>
                     </div>
                   </div>
-                </div>
-              )
-            }
-          </div>
+                )
+              }
+            </div>
+          </form>
         </div>
       </div>
 
