420346a99fdd5d8e81ca6450c922574857ded45b
ebuberoderick <ebuberoderick2@gmail.com>
2025-05-24 10:17:31 +0100

    ...

--- src/pages/subscription_details.jsx
+++ src/pages/subscription_details.jsx
@@ -11,16 +11,16 @@ function SubscriptionDetails() {
     return (
         <>
             <div className="relative">
-                <div className="p-6 z-40 sticky top-0 w-full">
+                <div className="p-6 z-20 sticky top-0 w-full">
                     <TopBar title="Welcome Diala 👋" />
                 </div>
                 <div className="w-96 fixed -top-36 -right-36 h-96 rounded-full bg-orange-400"></div>
                 <div className="w-96 fixed  top-72 -left-36 h-96 rounded-full bg-orange-400"></div>
                 <div className="w-[60vh] h-[60vh] fixed -bottom-52 right-12 rounded-full bg-orange-400"></div>
-                <div className="bg-amber-50/70 h-screen w-screen z-30 top-0 right-0 fixed backdrop-blur-3xl">
+                <div className="bg-amber-50/70 h-screen w-screen z-10 top-0 right-0 fixed backdrop-blur-3xl">
                 </div>
                 <div className='pb-24'>
-                    <div className="px-9 relative z-30">
+                    <div className="px-9 relative z-10">
                         <div className="relative md:px-4 pb-3">
                             <div onClick={() => router.back()} className="cursor-pointer flex gap-1 items-center top-0 left-12">
                                 <TfiAngleLeft /> Go Back
