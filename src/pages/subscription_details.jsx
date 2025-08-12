d6b0c54bd129b066c938991897f6514c725e860f
ebuberoderick <ebuberoderick2@gmail.com>
2025-08-12 16:55:43 +0100

    padding fix

--- src/pages/subscription_details.jsx
+++ src/pages/subscription_details.jsx
@@ -14,7 +14,7 @@ function SubscriptionDetails() {
     return (
         <>
             <div className="relative">
-                <div className="p-6 z-20 sticky top-0 w-full">
+                <div className="p-3 md:p-6 z-20 sticky top-0 w-full">
                     <TopBar title={`Welcome ${user?.value?.user?.fname} 👋`} />
                 </div>
                 <div className="w-96 fixed -top-36 -right-36 h-96 rounded-full bg-orange-400"></div>
@@ -23,7 +23,7 @@ function SubscriptionDetails() {
                 <div className="bg-amber-50/70 h-screen w-screen z-10 top-0 right-0 fixed backdrop-blur-3xl">
                 </div>
                 <div className='pb-24'>
-                    <div className="px-9 relative z-10">
+                    <div className="px-3 md:px-9 relative z-10">
                         <div className="relative md:px-4 pb-3">
                             <div onClick={() => router.back()} className="cursor-pointer flex gap-1 items-center top-0 left-12">
                                 <TfiAngleLeft /> Go Back
